abe0b2de3e082c4be83b50ae0e7d632df7197235
CalamityDeadshot <semyon.pyankov@gmail.com>
2022-05-22 22:05:56 +0300

    Initial setup: packages, hilt

---
+++ app/src/main/java/com/app/composetestingdemo/presentation/screens/registration/RegistrationScreen.kt
@@ -0,0 +1,8 @@
+package com.app.composetestingdemo.presentation.screens.registration
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RegistrationScreen() {
+
+}
---
+++ app/src/main/java/com/app/composetestingdemo/presentation/screens/registration/RegistrationViewModel.kt
@@ -0,0 +1,11 @@
+package com.app.composetestingdemo.presentation.screens.registration
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class RegistrationViewModel @Inject constructor(
+
+) : ViewModel() {
+}
